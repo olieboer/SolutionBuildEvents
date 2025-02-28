96dc4dc0e6f8d2cadf917e77886eafec689a5262
MarvinEcedo <marvin@ecedo.com>
2025-02-28 15:41:02 +0100

    added support for detecting the changing of build configuration, and having a separate event for that

--- SolutionBuildEvents/Parameter.cs
+++ SolutionBuildEvents/Parameter.cs
@@ -4,5 +4,6 @@
     {
         public string[] PreBuildEvent { get; set; } = new string[0];
         public string[] PostBuildEvent { get; set; } = new string[0];
+        public string[] ConfigurationChangedEvent { get; set; } = new string[0];
     }
 }
